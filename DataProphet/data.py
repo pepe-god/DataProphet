567825eab339dbce58ddb46a7ff1a6f840c15969
Arda <79140476+arda-h4sh@users.noreply.github.com>
2024-03-30 15:40:58 +0300

    Update data.py

--- DataProphet/data.py
+++ DataProphet/data.py
@@ -1,5 +1,15 @@
 import mysql.connector
 
+def get_user_input(field_name):
+    return input(f"{field_name} girin: ")
+
+def build_query(**kwargs):
+    conditions = [f"{field}='{value}'" for field, value in kwargs.items() if value]
+    return " AND ".join(conditions)
+
+def write_person_info(file, person):
+    file.write("\t".join(str(value) for value in person[1:]) + "\n")
+
 # MySQL bağlantısı oluşturma
 db = mysql.connector.connect(
     host="localhost",
@@ -11,57 +21,30 @@ db = mysql.connector.connect(
 # Veritabanı işlemleri için cursor oluşturma
 cursor = db.cursor()
 
-# Kullanıcıdan sorgu koşullarını girmesini iste
-tc = input("TC girin: ")
-adi = input("Adı girin: ")
-soyadi = input("Soyadı girin: ")
-dogum_tarihi = input("Doğum Tarihini girin (G-A-YYYY): ")
-nufus_il = input("Nüfus İlini girin: ")
-nufus_ilce = input("Nüfus İlçesini girin: ")
-anne_adi = input("Anne Adını girin: ")
-anne_tc = input("Anne TC'sini girin: ")
-baba_adi = input("Baba Adını girin: ")
-baba_tc = input("Baba TC'sini girin: ")
-uyruk = input("Uyruğu girin: ")
+# Kullanıcıdan sorgu koşullarını al
+query_conditions = {
+    "TC": get_user_input("TC"),
+    "ADI": get_user_input("Adı"),
+    "SOYADI": get_user_input("Soyadı"),
+    "DOGUMTARIHI": get_user_input("Doğum Tarihi (G-A-YYYY)"),
+    "NUFUSIL": get_user_input("Nüfus İli"),
+    "NUFUSILCE": get_user_input("Nüfus İlçesi"),
+    "ANNEADI": get_user_input("Anne Adı"),
+    "ANNETC": get_user_input("Anne TC'si"),
+    "BABAADI": get_user_input("Baba Adı"),
+    "BABATC": get_user_input("Baba TC'si"),
+    "UYRUK": get_user_input("Uyruk")
+}
 
-# Sorguyu hazırlama
-query = "SELECT * FROM `101m` WHERE"
-params = []
-
-if tc:
-    params.append(f" TC='{tc}'")
-if adi:
-    params.append(f" ADI='{adi}'")
-if soyadi:
-    params.append(f" SOYADI='{soyadi}'")
-if dogum_tarihi:
-    params.append(f" DOGUMTARIHI='{dogum_tarihi}'")
-if nufus_il:
-    params.append(f" NUFUSIL='{nufus_il}'")
-if nufus_ilce:
-    params.append(f" NUFUSILCE='{nufus_ilce}'")
-if anne_adi:
-    params.append(f" ANNEADI='{anne_adi}'")
-if anne_tc:
-    params.append(f" ANNETC='{anne_tc}'")
-if baba_adi:
-    params.append(f" BABAADI='{baba_adi}'")
-if baba_tc:
-    params.append(f" BABATC='{baba_tc}'")
-if uyruk:
-    params.append(f" UYRUK='{uyruk}'")
-
-query += " AND".join(params) + ";"
-
-# Sorguyu çalıştırma
+# Sorguyu hazırla ve çalıştır
+query = "SELECT * FROM `101m` WHERE " + build_query(**query_conditions)
 cursor.execute(query)
 
-# Sorgu sonuçlarını bir metin dosyasına yazma
-with open("data.txt", "w") as dosya:
+# Sonuçları dosyaya yaz
+with open("data.txt", "w", encoding="utf-8") as file:
     for row in cursor.fetchall():
-        satir = "\t".join(str(value) for value in row)  # Sütunları sekmeyle ayırarak birleştirme
-        dosya.write(satir + "\n")  # Her satırı dosyaya yazma
+        write_person_info(file, row)
 
-# Bağlantıyı kapatma
+# Bağlantıyı kapat
 cursor.close()
 db.close()
