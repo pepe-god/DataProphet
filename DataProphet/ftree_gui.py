b27b62225e3202a98c0f017cd07ceef24313ba11
PEPEGOD <79140476+pepe-god@users.noreply.github.com>
2024-10-08 01:55:00 +0300

    Update ftree_gui.py

--- DataProphet/ftree_gui.py
+++ DataProphet/ftree_gui.py
@@ -64,6 +64,114 @@ def write_summary(writer, summary_data):
     for key, value in summary_data.items():
         writer.writerow([key, value])
 
+def process_family_tree(cursor, tc_no, writer, prefix=""):
+    main_person = get_family_member_by_tc(cursor, tc_no)
+    if not main_person:
+        return
+
+    write_person_info(writer, main_person, prefix + "Ana Kayıt")
+
+    # Anne bilgileri
+    anne_tc = main_person[8]
+    anne_result = get_family_member_by_tc(cursor, anne_tc)
+    if anne_result:
+        write_person_info(writer, anne_result, prefix + "Anne")
+
+        # Anne'nin ebeveynleri (Büyükanne ve Büyükbaba)
+        anne_anne_tc = anne_result[8]
+        anne_baba_tc = anne_result[10]
+        anne_anne_result = get_family_member_by_tc(cursor, anne_anne_tc)
+        anne_baba_result = get_family_member_by_tc(cursor, anne_baba_tc)
+        if anne_anne_result:
+            write_person_info(writer, anne_anne_result, prefix + "Büyükanne (Anne'nin Anne)")
+        if anne_baba_result:
+            write_person_info(writer, anne_baba_result, prefix + "Büyükbaba (Anne'nin Baba)")
+
+    # Baba bilgileri
+    baba_tc = main_person[10]
+    baba_result = get_family_member_by_tc(cursor, baba_tc)
+    if baba_result:
+        write_person_info(writer, baba_result, prefix + "Baba")
+
+        # Baba'nın ebeveynleri (Büyükanne ve Büyükbaba)
+        baba_anne_tc = baba_result[8]
+        baba_baba_tc = baba_result[10]
+        baba_anne_result = get_family_member_by_tc(cursor, baba_anne_tc)
+        baba_baba_result = get_family_member_by_tc(cursor, baba_baba_tc)
+        if baba_anne_result:
+            write_person_info(writer, baba_anne_result, prefix + "Büyükanne (Baba'nın Anne)")
+        if baba_baba_result:
+            write_person_info(writer, baba_baba_result, prefix + "Büyükbaba (Baba'nın Baba)")
+
+    # Çocukları
+    cocuklari_result = get_children_by_parent_tc(cursor, main_person[1])
+    if cocuklari_result:
+        for cocuk in cocuklari_result:
+            write_person_info(writer, cocuk, prefix + "-Çocuk")
+
+            # Torunları
+            torunlari_result = get_children_by_parent_tc(cursor, cocuk[1])
+            if torunlari_result:
+                for torun in torunlari_result:
+                    write_person_info(writer, torun, prefix + f"{cocuk[2]} {cocuk[3]}'in Çocuğu")
+
+    # Kardeşleri
+    kardesleri_result = get_siblings_by_parent_tc(cursor, anne_tc, baba_tc, tc_no)
+    if kardesleri_result:
+        for kardes in kardesleri_result:
+            write_person_info(writer, kardes, prefix + "--Kardeşi")
+
+            # Yeğenleri
+            yegenleri_result = get_children_by_parent_tc(cursor, kardes[1])
+            if yegenleri_result:
+                for yegen in yegenleri_result:
+                    write_person_info(writer, yegen, prefix + "Yeğeni")
+
+                    # Yeğenin Çocuğu
+                    yegenin_cocugu_result = get_children_by_parent_tc(cursor, yegen[1])
+                    if yegenin_cocugu_result:
+                        for yegenin_cocugu in yegenin_cocugu_result:
+                            write_person_info(writer, yegenin_cocugu, prefix + f"{yegen[2]} {yegen[3]}'in Çocuğu")
+
+    # Dayı/Teyze ve Amca/Hala TC'lerini bir listeye topla
+    dayı_teyze_amca_hala_tc_list = []
+    dayı_teyze_result = []
+    amca_hala_result = []
+    if anne_result:
+        dayı_teyze_result = get_siblings_by_parent_tc(cursor, anne_result[8], anne_result[10], anne_result[1])
+        if dayı_teyze_result:
+            dayı_teyze_amca_hala_tc_list.extend([dayı_teyze[1] for dayı_teyze in dayı_teyze_result])
+            for dayı_teyze in dayı_teyze_result:
+                write_person_info(writer, dayı_teyze, prefix + "---Dayı/Teyze")
+    if baba_result:
+        amca_hala_result = get_siblings_by_parent_tc(cursor, baba_result[8], baba_result[10], baba_result[1])
+        if amca_hala_result:
+            dayı_teyze_amca_hala_tc_list.extend([amca_hala[1] for amca_hala in amca_hala_result])
+            for amca_hala in amca_hala_result:
+                write_person_info(writer, amca_hala, prefix + "---Amca/Hala")
+
+    # Tüm kuzenleri tek bir sorguda al
+    kuzenleri_result = get_cousins_by_parent_tc_list(cursor, dayı_teyze_amca_hala_tc_list)
+    if kuzenleri_result:
+        for kuzen in kuzenleri_result:
+            write_person_info(writer, kuzen, prefix + "Kuzen")
+
+            # Kuzenin Çocukları
+            kuzen_cocuklari_result = get_children_by_parent_tc(cursor, kuzen[1])
+            if kuzen_cocuklari_result:
+                for kuzen_cocugu in kuzen_cocuklari_result:
+                    write_person_info(writer, kuzen_cocugu, prefix + f"{kuzen[2]} {kuzen[3]}'in Çocuğu")
+
+    return {
+        "Kuzen Sayısı": len(kuzenleri_result) if kuzenleri_result else 0,
+        "Kardeş Sayısı": len(kardesleri_result) if kardesleri_result else 0,
+        "Yeğen Sayısı": len(yegenleri_result) if 'yegenleri_result' in locals() else 0,
+        "Çocuk Sayısı": len(cocuklari_result) if cocuklari_result else 0,
+        "Amca/Hala Sayısı": len(amca_hala_result) if amca_hala_result else 0,
+        "Dayı/Teyze Sayısı": len(dayı_teyze_result) if dayı_teyze_result else 0,
+        "Kuzen Çocukları Sayısı": sum([len(get_children_by_parent_tc(cursor, kuzen[1])) for kuzen in kuzenleri_result]) if kuzenleri_result else 0,
+    }
+
 def process_tc_number(tc_no):
     db_config = read_config()
     cnx = connect_to_database(db_config)
@@ -91,112 +199,27 @@ def process_tc_number(tc_no):
             writer = csv.writer(file)
             writer.writerow(["Kategori", "TC", "Adı", "Soyadı", "Doğum Tarihi", "Nufus İli", "Nufus İlçesi", "Anne Adı", "Anne TC", "Baba Adı", "Baba TC", "Uyruk"])
 
-            # Ana kayıt
-            write_person_info(writer, main_person, "Ana Kayıt")
+            # Ana kişinin kendi sülalesi
+            writer.writerow([])
+            writer.writerow([f"Kendi Sülalesi"])
+            summary_data_kendi = process_family_tree(cursor, tc_no, writer, f"")
+            write_summary(writer, summary_data_kendi)
 
-            # Anne bilgileri
+            # Ana kişinin annesinin sülalesi
             anne_tc = main_person[8]
-            anne_result = get_family_member_by_tc(cursor, anne_tc)
-            if anne_result:
-                write_person_info(writer, anne_result, "Anne")
+            if anne_tc:
+                writer.writerow([])
+                writer.writerow([f"Annesinin Sülalesi"])
+                summary_data_anne = process_family_tree(cursor, anne_tc, writer, f"")
+                write_summary(writer, summary_data_anne)
 
-                # Anne'nin ebeveynleri (Büyükanne ve Büyükbaba)
-                anne_anne_tc = anne_result[8]
-                anne_baba_tc = anne_result[10]
-                anne_anne_result = get_family_member_by_tc(cursor, anne_anne_tc)
-                anne_baba_result = get_family_member_by_tc(cursor, anne_baba_tc)
-                if anne_anne_result:
-                    write_person_info(writer, anne_anne_result, "Büyükanne (Anne'nin Anne)")
-                if anne_baba_result:
-                    write_person_info(writer, anne_baba_result, "Büyükbaba (Anne'nin Baba)")
-
-            # Baba bilgileri
+            # Ana kişinin babasının sülalesi
             baba_tc = main_person[10]
-            baba_result = get_family_member_by_tc(cursor, baba_tc)
-            if baba_result:
-                write_person_info(writer, baba_result, "Baba")
-
-                # Baba'nın ebeveynleri (Büyükanne ve Büyükbaba)
-                baba_anne_tc = baba_result[8]
-                baba_baba_tc = baba_result[10]
-                baba_anne_result = get_family_member_by_tc(cursor, baba_anne_tc)
-                baba_baba_result = get_family_member_by_tc(cursor, baba_baba_tc)
-                if baba_anne_result:
-                    write_person_info(writer, baba_anne_result, "Büyükanne (Baba'nın Anne)")
-                if baba_baba_result:
-                    write_person_info(writer, baba_baba_result, "Büyükbaba (Baba'nın Baba)")
-
-            # Çocukları
-            cocuklari_result = get_children_by_parent_tc(cursor, main_person[1])
-            if cocuklari_result:
-                for cocuk in cocuklari_result:
-                    write_person_info(writer, cocuk, "-Çocuk")
-
-                    # Torunları
-                    torunlari_result = get_children_by_parent_tc(cursor, cocuk[1])
-                    if torunlari_result:
-                        for torun in torunlari_result:
-                            write_person_info(writer, torun, f"{cocuk[2]} {cocuk[3]}'in Çocuğu")
-
-            # Kardeşleri
-            kardesleri_result = get_siblings_by_parent_tc(cursor, anne_tc, baba_tc, tc_no)
-            if kardesleri_result:
-                for kardes in kardesleri_result:
-                    write_person_info(writer, kardes, "--Kardeşi")
-
-                    # Yeğenleri
-                    yegenleri_result = get_children_by_parent_tc(cursor, kardes[1])
-                    if yegenleri_result:
-                        for yegen in yegenleri_result:
-                            write_person_info(writer, yegen, "Yeğeni")
-
-                            # Yeğenin Çocuğu
-                            yegenin_cocugu_result = get_children_by_parent_tc(cursor, yegen[1])
-                            if yegenin_cocugu_result:
-                                for yegenin_cocugu in yegenin_cocugu_result:
-                                    write_person_info(writer, yegenin_cocugu, f"{yegen[2]} {yegen[3]}'in Çocuğu")
-
-            # Dayı/Teyze ve Amca/Hala TC'lerini bir listeye topla
-            dayı_teyze_amca_hala_tc_list = []
-            dayı_teyze_result = []
-            amca_hala_result = []
-            if anne_result:
-                dayı_teyze_result = get_siblings_by_parent_tc(cursor, anne_result[8], anne_result[10], anne_result[1])
-                if dayı_teyze_result:
-                    dayı_teyze_amca_hala_tc_list.extend([dayı_teyze[1] for dayı_teyze in dayı_teyze_result])
-                    for dayı_teyze in dayı_teyze_result:
-                        write_person_info(writer, dayı_teyze, "---Dayı/Teyze")
-            if baba_result:
-                amca_hala_result = get_siblings_by_parent_tc(cursor, baba_result[8], baba_result[10], baba_result[1])
-                if amca_hala_result:
-                    dayı_teyze_amca_hala_tc_list.extend([amca_hala[1] for amca_hala in amca_hala_result])
-                    for amca_hala in amca_hala_result:
-                        write_person_info(writer, amca_hala, "---Amca/Hala")
-
-            # Tüm kuzenleri tek bir sorguda al
-            kuzenleri_result = get_cousins_by_parent_tc_list(cursor, dayı_teyze_amca_hala_tc_list)
-            if kuzenleri_result:
-                for kuzen in kuzenleri_result:
-                    write_person_info(writer, kuzen, "Kuzen")
-
-                    # Kuzenin Çocukları
-                    kuzen_cocuklari_result = get_children_by_parent_tc(cursor, kuzen[1])
-                    if kuzen_cocuklari_result:
-                        for kuzen_cocugu in kuzen_cocuklari_result:
-                            write_person_info(writer, kuzen_cocugu, f"{kuzen[2]} {kuzen[3]}'in Çocuğu")
-
-            # Summary
-            summary_data = {
-                "Kuzen Sayısı": len(kuzenleri_result) if kuzenleri_result else 0,
-                "Kardeş Sayısı": len(kardesleri_result) if kardesleri_result else 0,
-                "Yeğen Sayısı": len(yegenleri_result) if 'yegenleri_result' in locals() else 0,
-                "Çocuk Sayısı": len(cocuklari_result) if cocuklari_result else 0,
-                "Amca/Hala Sayısı": len(amca_hala_result) if amca_hala_result else 0,
-                "Dayı/Teyze Sayısı": len(dayı_teyze_result) if dayı_teyze_result else 0,
-                "Kuzen Çocukları Sayısı": sum([len(get_children_by_parent_tc(cursor, kuzen[1])) for kuzen in kuzenleri_result]) if kuzenleri_result else 0,
-            }
-
-            write_summary(writer, summary_data)
+            if baba_tc:
+                writer.writerow([])
+                writer.writerow([f"Babasının Sülalesi"])
+                summary_data_baba = process_family_tree(cursor, baba_tc, writer, f"")
+                write_summary(writer, summary_data_baba)
 
         messagebox.showinfo("Başarılı", f"Veriler {filename} dosyasına kaydedildi.")
     else:
